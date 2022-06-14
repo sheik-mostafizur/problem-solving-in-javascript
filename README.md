cf86412c49cd0c67167186f44448ba21ea58a30a
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2022-06-14 06:38:29 +0600

    Lecture 001

---
+++ README.md
@@ -0,0 +1,3 @@
+# problem-solving-in-javascript
+
+Lecture 001. function in javascript.
---
+++ lecture_001/app.js
@@ -0,0 +1,24 @@
+// function funName(){
+//   console.log('Hello World!');
+// }
+
+// funName()
+
+// let num1 = 5;
+// let num2 = 2;
+// let total = num1 + num2;
+// console.log(total);
+
+// let num3 = 5;
+// let num4 = 4;
+// let total1 = num3 + num4;
+// console.log(total1);
+
+function add(num1, num2) {
+  let total = num1 + num2;
+  return total;
+}
+
+console.log(add(5, 2));
+console.log(add(5, 5));
+console.log(add(5, 15));
