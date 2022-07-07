825b8cbc7774cdfc4e89f17db02c356591510b70
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2022-07-07 18:54:30 +0600

    Lecture 04

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Lecture 001. Function in javascript
 Lecture 002. Return the given input from the function
 
 Lecture 003. Write a sum sub mul div function that takes two numbers
+
+Lecture 004. Write a function that any products price rate calculate
---
+++ lecture_004/app.js
@@ -0,0 +1,12 @@
+// Write a function that any products price rate calculate
+// Products =
+// Product Price =
+// Total Price =
+
+function productPrice(products, price) {
+  return products * price;
+}
+
+let result = productPrice(5, 500);
+
+console.log(result);
